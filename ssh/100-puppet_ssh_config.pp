8e3c2a6ad5ed06a64fee9af61df5c60a0f862ebe
Janviere-dev <j.munezero@alustudent.com>
2025-07-16 11:39:34 -0700

    Puppet to make changes to our ssh  configuration file.

--- ssh/100-puppet_ssh_config.pp
+++ ssh/100-puppet_ssh_config.pp
@@ -2,13 +2,13 @@
 
 file_line { 'Declare identity file':
   path  => '/etc/ssh/ssh_config',
-  line  => '    IdentityFile ~/.ssh/school',
-  match => '^\s*IdentityFile',
+  line  => 'IdentityFile ~/.ssh/school',
+  match => '^IdentityFile',
 }
 
-file_line { 'Turn off passwd auth':
+file_line { 'Turn off password auth':
   path  => '/etc/ssh/ssh_config',
-  line  => '    PasswordAuthentication no',
-  match => '^\s*PasswordAuthentication',
+  line  => 'PasswordAuthentication no',
+  match => '^PasswordAuthentication',
 }
 
